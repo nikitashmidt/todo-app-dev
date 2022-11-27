872d11c012adb9f153133ccb20d47450aaa33121
Nikita Shmidt <nikitka.shmidt.01@gmail.com>
2022-11-27 18:17:46 +0600

    changes

--- js/main.js
+++ js/main.js
@@ -345,6 +345,9 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       taskInput.focus();
     }, 600);
+    checkbox.onclick = () => {
+      taskInput.focus()
+    }
     disableScroll();
     transition("-50%");
   }
@@ -353,9 +356,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (modalDelete.classList.contains("modal-delete-active"))
       modalDelete.classList.remove("modal-delete-active");
     if (modal.classList.contains("modal-task-active"))
-      setTimeout(() => {
         modal.classList.remove("modal-task-active");
-      }, 200)
     if (modalTrash.classList.contains("modal-trash-active"))
       modalTrash.classList.remove("modal-trash-active");
     if (modalComments.classList.contains("modal-comments-active")) {
