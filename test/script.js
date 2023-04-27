4c873960856e969750235d1d5f99c9a16beb850e
Nikita Shmidt <71144807+nikitashmidt@users.noreply.github.com>
2023-04-27 11:14:27 +0600

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,9 +1,11 @@
 const titles = document.querySelectorAll(".title");
 const hamburger = document.querySelector('.hamburger');
 const menuList = document.querySelector('.menu-list');
-const blocks = document.querySelectorAll('.block');
-
+const blocks = document.querySelectorAll('.block').length;
+const overlay = document.querySelector('.overlay');
+const menuSvg = document.querySelector('.menu-list-svg');
 const count = document.querySelector('.count');
+
 if (count) {
  count.innerHTML = `Всего вопросов ${blocks} / 20`;
 }
@@ -12,18 +14,30 @@ titles.forEach((title) => {
     title.addEventListener("click", boxHandler); 
 });
 
-hamburger.addEventListener('click', () => {
+function toggleClasses(e) {
   menuList.classList.toggle('menu-list-active');
-  hamburger.classList.toggle('hamburger-active');
-})
+  overlay.classList.toggle('overlay-active');
+}
+
+hamburger.addEventListener('click',toggleClasses)
+overlay.addEventListener('click',toggleClasses)
+menuSvg.addEventListener('click',toggleClasses)
 
 function boxHandler(e) {
 
   let currentBox = e.target.closest(".block"); 
 
+  const currentValue = e.target.textContent;
+
   let currentContent = e.target.nextElementSibling; 
 
   currentBox.classList.toggle("active"); 
+
+  if (currentBox.classList.contains('active')) {
+    count.innerHTML = currentValue;
+  } else {
+    count.innerHTML = `Всего вопросов ${blocks} / 20`;
+  }
 
   if (currentBox.classList.contains("active")) {
     currentContent.style.maxHeight = currentContent.scrollHeight + "px"; 
